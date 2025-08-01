af387cf746e5513106a607deaa8757f96f1ef238
knighthot <fikramulbizli@gmail.com>
2025-08-01 21:37:16 +0700

    add tailwind

--- App.tsx
+++ App.tsx
@@ -1,28 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+import { View, Text } from 'react-native'
+import React from 'react'
+import "./global.css"
+const App = () => {
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+    <View className="flex-1 items-center justify-center">
+      <Text className='text-3xl font-bold bg-red-500'>App</Text>
+      <View className='bg-white'>
+        <Text className='text-3xl font-bold'>App</Text>
+      </View>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
